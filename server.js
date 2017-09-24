858b0041a9f6b8108a4922f8b0f1487bf4412084
reneegeffre <renee.geffre@gmail.com>
2017-09-24 16:22:54 -0400

    fixing port settings

--- server.js
+++ server.js
@@ -156,6 +156,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
